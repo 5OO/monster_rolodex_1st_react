e7ecce9ad689b6fa6ee5f69d7d3505e9b2139948
JaanMürk <jaan.murk@avalanchelabs.com>
2022-12-19 17:01:08 +0200

    refactoring - cleaning not used code

--- src/App.js
+++ src/App.js
@@ -12,42 +12,9 @@ class App extends Component {
         };
     }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then((response) => response.json())
-            .then((users) =>
-                this.setState(
-                    () => {
-                        return {monsters: users};
-                    },
-                    () => {
-                        console.log(this.state);
-                    }
-                )
-            );
-    }
-
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Hi {this.state.name.firstName} {this.state.name.lastName}, Ma töötan ettevõttes {this.state.company}!
-                    </p>
-                    <button onClick={()=> {
-                        this.setState(
-                            ()=>{
-                                return {
-                                name: {firstName: 'Sigri', lastName:'Migri'},
-                                };
-                            },()=>{
-                                console.log(this.state);
-                            }
-                        );
-                    }}
-                    >Change name</button>
-                </header>
             </div>
         );
     }

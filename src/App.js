c82631214027406a07bab9ae12923ee9a73bee77
JaanMürk <jaan.murk@avalanchelabs.com>
2022-12-19 13:50:39 +0200

    setting state with an object that wea are passing on.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            name: {firstName: 'Snata', lastName: 'klaus'},
+            company: 'misiganes on su nimi'
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) =>
+                this.setState(
+                    () => {
+                        return {monsters: users};
+                    },
+                    () => {
+                        console.log(this.state);
+                    }
+                )
+            );
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                        <br/>
-                        Tämä on ensimmäinen REACT app jonka olen ikinä tehnyt.
+                        Hi {this.state.name.firstName} {this.state.name.lastName}, Ma töötan ettevõttes {this.state.company}!
                     </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
+                    <button onClick={()=> {
+                        this.setState({name: {firstName: 'Sigri', lastName:'Migri'}});
+                        console.log(this.state);
+                    }}
+                    >Change name</button>
                 </header>
             </div>
         );

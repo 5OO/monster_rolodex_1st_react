fbf04b5ad93e5eddfd573d3225be0cc675c58987
JaanMürk <jaan.murk@avalanchelabs.com>
2022-12-20 17:42:10 +0200

    storing original data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 
         this.state = {
             monsters: [],
+            searchField: ''
         };
         console.log('constructor')
     }
@@ -29,26 +30,30 @@ class App extends Component {
 
     render() {
         console.log('render')
+        const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+        });
+
         return (
             <div className="App">
-                <input className='search-box' type='search' placeholder='search monsters'
-                       onChange={(event) => {
-                           console.log(event.target.value);
-                           const searchString = event.target.value.toLocaleLowerCase();
-                           const filteredMonsters = this.state.monsters.filter((monster) => {
-                               return monster.name.toLocaleLowerCase().includes(searchString);
-                           });
-                           this.setState(()=> {
-                               return {monsters: filteredMonsters};
-                           })
-                       }}
+                <input
+                    className='search-box'
+                    type='search'
+                    placeholder='search monsters'
+                    onChange={(event) => {
+                        const searchField = event.target.value.toLocaleLowerCase();
+
+                        this.setState(() => {
+                            return {searchField};
+                        })
+                    }}
                 />
 
                 {
-                    this.state.monsters.map((monster) => {
-                        return <div key={monster.id}>
+                    filteredMonsters.map((monster) => {
+                        return (<div key={monster.id}>
                             <h1>    {monster.name}</h1>
-                        </div>;
+                        </div>);
                     })
                 }
             </div>

3210cfed8beb1abee81e8d9fa6677b3d153d4b1e
JaanMürk <jaan.murk@avalanchelabs.com>
2022-12-20 15:27:27 +0200

    adding keys to Array for mapping an element;

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ class App extends Component {
         this.state = {
             monsters: [
                 {
+                    id: '23huh32o',
                     name: 'Linda'
                 },
                 {
+                    id: '4j8wefw0sa',
                     name: 'Frank'
                 },
                 {
+                    id: 'x8cnrycn80c',
                     name: 'Jackie'
                 },
                 {
+                    id: 'x849ytnxq',
                     name: 'Ljoša'
                 }
             ]
@@ -27,8 +31,10 @@ class App extends Component {
         return (
             <div className="App">
                 {
-                    this.state.monsters.map((monster)=>{
-                        return <h1>{monster.name}</h1>
+                    this.state.monsters.map((monster) => {
+                        return <div key={monster.id}>
+                            <h1>    {monster.name}</h1>
+                        </div>;
                     })
                 }
             </div>

382f054784352cd54218a72e2d2f83fa914853c8
JaanMürk <jaan.murk@avalanchelabs.com>
2022-12-21 16:35:06 +0200

    extracting card to separate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {Component} from "react";
-import CardList from "./card-list/card-list.component";
-import SearchBox from "./search-box/search-box.component";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 import './App.css';
 
 class App extends Component {
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,10 @@
+import {Component} from "react";
+
+import './card.styles.css';
+
+class Card extends Component {
+
+
+}
+
+export default Card;

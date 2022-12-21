2190e25a784eeaa735e5d3e0423112850a824529
JaanMürk <jaan.murk@avalanchelabs.com>
2022-12-21 11:04:47 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -56,14 +56,7 @@ class App extends Component {
                     onChange={onSearchChange}
                 />
 
-                {/*{*/}
-                {/*    filteredMonsters.map((monster) => {*/}
-                {/*        return (<div key={monster.id}>*/}
-                {/*            <h1>    {monster.name}</h1>*/}
-                {/*        </div>);*/}
-                {/*    })*/}
-                {/*}*/}
-                <CardList monsters={filteredMonsters} />
+                <CardList monsters ={filteredMonsters}/>
             </div>
         );
     }

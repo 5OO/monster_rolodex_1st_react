4ad79aecf880c4bb4f3126eb4ae4080c4652ff50
JaanMürk <jaan.murk@avalanchelabs.com>
2022-12-21 10:55:53 +0200

    we passed a prop named monster (key) with its string value to a card-list component .

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
                 {/*        </div>);*/}
                 {/*    })*/}
                 {/*}*/}
-                <CardList />
+                <CardList monsters={'I am the monsters'}/>
             </div>
         );
     }

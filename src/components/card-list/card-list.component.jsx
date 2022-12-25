69244274a5529bd8ad00e5ae69d841c16a8ea547
5OO <git.hub@myrk.eu>
2022-12-25 21:45:40 +0200

    Most of the time you're just going to use props, and this props is the exact same as this props.
    It's an object that has all of the different parameters and properties essentially that are being passed
    
    into this component.
    
    It's just that instead of this props, it's going to be the first parameter that gets passed into this
    
    functional component.
    
    Similarly, because we know that this will always be the first parameter, we actually don't need to
    
    do the structuring here.
    
    We can do the structuring right inside of the parameter itself, like so.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,22 +1,18 @@
 import './card-list.styles.css'
 import Card from "../card/card.component";
 
-const CardList = (props) => {
+const CardList = ({monsters}) => {
+    return (
 
+        <div className="card-list">
+            {monsters.map((monster) => {
 
-        const {monsters} = props;
-        return (
-
-            <div className="card-list">
-                {monsters.map((monster) => {
-
-                    return (
-                        <Card monster={monster}/>
-                    );
-                })}
-            </div>
-        );
-
+                return (
+                    <Card monster={monster}/>
+                );
+            })}
+        </div>
+    );
 }
 
 export default CardList;

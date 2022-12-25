87a4c3060bf40f2eb7d46680d72b51182a79e6ba
5OO <git.hub@myrk.eu>
2022-12-25 21:32:03 +0200

    And then what I'm going to do is I am going to call another use effect. ln 19
    This filtered monster's call.
    
    And here I'm going to say this is new filtered monsters.
    
    And once we filter through and we have that new monster, then we are going to say set filtered monsters
    
    to the new filtered monsters.
    
    And now filtered monsters here is always going to contain the values we want.
    
    And what I'm going to do is I'm going to say, I want you to filter through these monsters whenever
    
    either the monsters array changes or whenever the search field changes.
    
    so video jätk # 60 osast.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react";
+import {useEffect, useState} from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import './App.css';
@@ -6,6 +6,7 @@ import './App.css';
 const App = () => {
     const [searchField, setSearchField] = useState('');   // [value, setValue] one value, one hook
     const [monsters, setMonsters] = useState([]);
+    const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
     console.log(' renderdame ! ')
 
@@ -15,16 +16,19 @@ const App = () => {
             .then((json) => setMonsters(json));
     }, []);
 
-
+    useEffect(() => {
+        const newFilteredMonsters = monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchField);
+        });
+        setFilteredMonsters(newFilteredMonsters);
+    }, [monsters, searchField]);
 
     const onSearchChange = (event) => {
         const searchFieldString = event.target.value.toLocaleLowerCase();
         setSearchField(searchFieldString);
     }
 
-            const filteredMonsters = monsters.filter((monster) => {
-            return monster.name.toLocaleLowerCase().includes(searchField);
-        });
+
 
     return (
         <div className="App">
